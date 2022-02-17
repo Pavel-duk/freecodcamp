69bf45a32651202da3072280abd4a25cf36c5dc9
Pavel-duk <www.duke3d@mail.ru>
2022-02-18 00:33:14 +0300

    18.02.2022

--- codewars.js
+++ codewars.js
@@ -1,2 +1,27 @@
-console.log("Hello world")
-console.log("Hello world")
+//функция проверяет чтобы пин был 4 или 6 цифр, а также состоял только из цифр. Если все выполняется выдает true, в противном случае выдает false 
+function validatePIN (pin) {
+    let list = ["0","1","2","3","4","5","6","7","8","9"]
+    if (pin == '') {
+        return false
+      }
+    for(var i = 0; i < pin.length; i++) {
+      if (list.indexOf(pin[i]) == -1){
+        return false
+      }
+      if (pin.length < 4) {
+        return false
+      }
+      if (pin.length == 5) {
+        return false
+      }
+      if (pin.length > 6) {
+        return false
+      }
+    }
+    if (pin.length == 4){
+      return true 
+    }
+    if (pin.length == 6){
+      return true 
+    }
+}
