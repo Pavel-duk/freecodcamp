d860ce5550f4d635df3b78c364aa84a02c93547c
Pavel-duk <www.duke3d@mail.ru>
2023-05-19 05:38:51 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -115,3 +115,13 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+//26
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+//27
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
