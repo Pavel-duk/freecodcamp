3efa73b02dde8bc06986b34763fdb22a850fdd64
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 14:32:52 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -398,3 +398,31 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+//59
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >=25) {
+    
+      return "Yes";
+    
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+//60
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
