886b1ef2bf424e284586c5d87e68373d534d2b89
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 14:04:58 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -302,3 +302,13 @@ function testEqual(val) {
 
 testEqual(10);
 }
+//51
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
