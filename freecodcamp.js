d4d2fadfad7e8678fd4d235741e697666c253de6
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 12:56:01 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -175,3 +175,9 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 myArray.push(["dog",3])
+//37
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
