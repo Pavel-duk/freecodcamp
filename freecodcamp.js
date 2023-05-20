762610d4b98e72cacb7fe534d1ae991854399f13
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 12:30:24 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -125,3 +125,9 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+//28
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
