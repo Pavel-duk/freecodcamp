e0d8b4076b10ae3a65100761ae286e43e7c8b143
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 13:46:49 +0300

    ad task

--- freecodcamp.js
+++ freecodcamp.js
@@ -272,3 +272,12 @@ function addFive() {
 
 addThree();
 addFive();
+//48
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7);
+// Only change code below this line
