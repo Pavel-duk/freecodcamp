b96e3a5a1eb89733e46e96189cb7b630dca442ba
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 12:52:27 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -160,3 +160,12 @@ var myData = myArray[0];
 const myArray = [18, 64, 99];
 myArray[0] = 45;
 // Only change code below this line
+//35
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
