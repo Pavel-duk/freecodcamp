5018490a8bb7d3a5855b060cc79d984ef31730d8
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 13:40:31 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -232,3 +232,14 @@ function fun2() {
   }
   console.log(output);
 }
+//45
+function myLocalScope() {
+  // Only change code below this line
+  var myVar = 1;
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
