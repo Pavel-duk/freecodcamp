44be9e875c9c4fbfacd61cc26ad08a35785c8241
Pavel-duk <www.duke3d@mail.ru>
2023-05-20 13:42:26 +0300

    add task

--- freecodcamp.js
+++ freecodcamp.js
@@ -243,3 +243,15 @@ myLocalScope();
 // Run and check the console
 // myVar is not defined outside of myLocalScope
 console.log('outside myLocalScope', myVar);
+//46
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
